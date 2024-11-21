b87bce89d05ef0006523b185fe94955f945e85a4
Abhishek Gupta <91731524+AbhiGupta8295@users.noreply.github.com>
2024-11-21 11:25:23 +0530

    Update Dump.py

--- Aditya/Dump.py
+++ Aditya/Dump.py
@@ -1,11 +1,6 @@
-global:
-  scrape_interval: 15s
-
-scrape_configs:
-  - job_name: "node_exporter"
-    static_configs:
-      - targets: ["node-exporter:9100"]
-
-  - job_name: "webserver"
-    static_configs:
-      - targets: ["webserver:8080"]
+apiVersion: 1
+datasources:
+  - name: Prometheus
+    type: prometheus
+    access: proxy
+    url: http://prometheus:9090
